dde53cbe8cc3d247913ea06e132e75f82f17fd44
Yohei Iino <wheatandcat@gmail.com>
2020-03-14 20:33:22 +0900

    ログインからFirestore接続まで実装 (#5)
    * add: auth (#2)
    
    * add: firestore conect
    
    * add: firestore conect
    
    * connect: calendar firestore

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Calendar />
+    <Calendar :calendars="calendars" />
   </div>
 </template>
 
@@ -16,11 +16,40 @@
 
 <script>
 import Vue from 'vue'
+import { findInID as findItemInID } from 'peperomia-util/build/firestore/item'
+import { findByUID as findCalendarByUID } from 'peperomia-util/build/firestore/calendar'
 import Calendar from '~/components/templates/dashboard/calendar.vue'
 
 export default Vue.extend({
   components: {
     Calendar,
+  },
+  data: () => ({
+    calendars: [],
+  }),
+  async mounted() {
+    const uid = this.$store.state.authUser.uid
+    const firestore = this.$fireStore
+
+    const calendars = await findCalendarByUID(firestore, uid)
+    if (calendars.length === 0) {
+      return
+    }
+
+    const ids = calendars.map(calendar => String(calendar.id))
+    const items = await findItemInID(firestore, uid, ids)
+    const result = calendars.map(calendar => {
+      const item = items.find(v => v.id === calendar.id)
+
+      return {
+        ...calendar,
+        ...item,
+      }
+    })
+
+    console.log(result)
+
+    this.calendars = result
   },
 })
 </script>
