c36cb699f894aebc8e03aa8b573581fd2826cb8b
wheatandcat <wheatandcat@gmail.com>
2020-03-31 23:50:39 +0900

    test

--- components/molecules/calendar/scheduleDay.vue
+++ components/molecules/calendar/scheduleDay.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="root" :style="bg">
     <div class="event" :style="fontStyle">
-      {{ dayjs(props).format('D') }}
+      {{ dayjs(props.date).format('D') }}
     </div>
     <div>
       <v-img :src="props.kindData.src" class="kind-img" :style="imgStyle" />
