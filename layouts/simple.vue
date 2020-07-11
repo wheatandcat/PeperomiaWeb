2c74b0e9ab19e96106d6c51010b2bda4a6a93319
wheatandcat <wheatandcat@gmail.com>
2020-07-12 00:03:47 +0900

    add: share

--- layouts/simple.vue
+++ layouts/simple.vue
@@ -1,5 +1,29 @@
 <template>
   <v-app dark>
+    <v-app-bar color="primary" fixed app>
+      <img src="/logo.png" alt="logp" class="logo" />
+
+      <v-spacer />
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon color="white">mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item href="https://peperomia.app/" target="_blank">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>ペペロミア</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
     <v-content class="content">
       <v-container>
         <nuxt />
@@ -16,5 +40,10 @@
 
 .content {
   background-color: #eee;
+}
+
+.logo {
+  width: 30px;
+  height: 30px;
 }
 </style>
--- use/useShareItem.ts
+++ use/useShareItem.ts
@@ -1,4 +1,37 @@
-/*
 import { reactive, SetupContext, toRefs } from '@vue/composition-api'
-import useShareItemQuery from '~/queries/useShareItem'
-*/
+import shareItemQuery from '~/queries/shareItem.gql'
+import { ShareItemQuery, ShareItemQueryVariables } from '~/queries/types'
+
+type UseFetchShareItemState = {
+  item: ShareItemQuery['item'] | null
+  loading: boolean
+}
+
+const useFetchShareItem = (ctx: SetupContext) => {
+  const state = reactive<UseFetchShareItemState>({
+    item: null,
+    loading: true,
+  })
+
+  const fetchShareItem = async (itemId: string) => {
+    state.loading = true
+    const res = await ctx.root.$apollo.query<
+      ShareItemQuery,
+      ShareItemQueryVariables
+    >({
+      query: shareItemQuery,
+      fetchPolicy: 'network-only',
+      variables: { itemId },
+    })
+
+    state.item = res.data.item
+    state.loading = false
+  }
+
+  return {
+    ...toRefs(state),
+    fetchShareItem,
+  }
+}
+
+export default useFetchShareItem
