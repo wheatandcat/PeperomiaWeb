b8cb2d9b377fdb5e675d0613e700fb4bb36dc7c6
Yohei Iino <wheatandcat@gmail.com>
2021-01-06 00:10:08 +0900

    nuxt-community/storybookの置き換える (#58)
    * add: @nuxtjs/storybook
    
    * update: storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,10 @@
-
-
 import { configure, addDecorator } from '@storybook/vue'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VueCompositionApi from '@vue/composition-api'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import theme from '~/plugins/vuetify'
+import theme from '../plugins/vuetify.ts'
 
 Vue.use(Vuex)
 Vue.use(VueCompositionApi)
@@ -25,7 +23,7 @@ addDecorator(() => ({
 
 const req = require.context('../components', true, /.stories.js$/)
 function loadStories() {
-  req.keys().forEach(filename => req(filename))
+  req.keys().forEach((filename) => req(filename))
 }
 
 configure(loadStories, module)
